b648407255aeccec09b2109c04a598057c89f49c
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-18 10:05:01 -0600

    modified:   resources/views/layouts/vistapadre.blade.php 	modified:   resources/views/periodo/periodo.blade.php

--- resources/views/layouts/vistapadre.blade.php
+++ resources/views/layouts/vistapadre.blade.php
@@ -165,9 +165,7 @@
       <div class="container-fluid page-body-wrapper">
         <!-- partial:../../partials/_navbar.html -->
         <nav class="navbar p-0 fixed-top d-flex flex-row">
-          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a>
-          </div>
+   
           <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
               <span class="mdi mdi-menu"></span>
@@ -211,6 +209,28 @@
                 </div>
               </li>
             </ul>
+            <!-- barra de menu en modo celular -->
+            <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
+          <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+            <!-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="" alt="logo" /></a> -->
+          </div>
             <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
               <span class="mdi mdi-format-line-spacing"></span>
             </button>
