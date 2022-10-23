0c20cad7c11873455d7c02d891321b83b3764c9e
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-22 20:24:58 -0600

    modified:   resources/views/Auth/preguntas.blade.php

--- resources/views/Auth/preguntas.blade.php
+++ resources/views/Auth/preguntas.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
     <head>
-        
         <style>
             body{
                 background-image: url('/assets/images/playa1.jpeg');
@@ -13,8 +12,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HTOURS | RECUPERACION</title>
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- FAVICON -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/HTOURS.png')}}" />
     </head>
 
     <body>
