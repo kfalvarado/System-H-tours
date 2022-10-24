26ad74bcd49db1b913e4ba3c5315f61f1556b2db
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-24 00:03:17 -0600

    modified:   resources/views/layouts/vistapadre.blade.php 	modified:   resources/views/resultado/Resultado.blade.php

--- resources/views/layouts/vistapadre.blade.php
+++ resources/views/layouts/vistapadre.blade.php
@@ -7,7 +7,7 @@
   <title>@yield('titulo')</title>
 
   <!-- Bootstrap  -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
--- resources/views/resultado/Resultado.blade.php
+++ resources/views/resultado/Resultado.blade.php
@@ -2,7 +2,7 @@
 
 <!-- titulo de la pagina  -->
 @section('titulo')
-Balance General
+Libro Mayor | inicio
 @endsection
 <!-- foto de la barra lateral debajo del nombre HTOURS  -->
 @section('foto-user1')
@@ -29,231 +29,138 @@ Emerson
 <!-- contenido de la pagina  -->
 @section('contenido')
 
-
-
-    <!-- inicio de menu  -->
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:../../partials/_navbar.html -->
-      <nav class="navbar p-0 fixed-top d-flex flex-row">
-        <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-          <a class="navbar-brand brand-logo-mini" href="../../index.html"><H3><font color="white">SHT</font></H3></a>
+<!-- INICIO MODAL PARA NUEVA  -->
+<div class="modal-container">
+  <div class="modal fade bd-example-modal-lg" id="dialogo1">
+    <!-- COLOCARLE UN lg PARA TAMANO MEDIANO COLOCARLE UN sm PARA TAMANO PEQUENO -->
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <!-- CABECERA DEL DIALOGO NUEVA-->
+        <div class="modal-header">
+          <h4 class="modal-title">
+            <center> Seleccionar Periodo</center>
+          </h4>
+          <!-- <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button> -->
         </div>
-        <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
-          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-            <span class="mdi mdi-menu"></span>
-          </button>
-          <!-- <ul class="navbar-nav w-100">
-            <li class="nav-item w-100">
-              <form class="nav-link mt-2 mt-md-0 d-none d-lg-flex search">
-                <input type="text" class="form-control" placeholder="Search products">
-              </form>
-            </li> -->
-          </ul>
-          <ul class="navbar-nav navbar-nav-right">
-            <li class="nav-item dropdown d-none d-lg-block">
-           
-            <li class="nav-item dropdown">
-              <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
-                <div class="navbar-profile">
-                  <img class="img-xs rounded-circle" src="../../assets/images/faces/usuario1.png" alt="">
-                  <p class="mb-0 d-none d-sm-block navbar-profile-name">Usuario1</p>
-                  <i class="mdi mdi-menu-down d-none d-sm-block"></i>
-                </div>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
-                <h6 class="p-3 mb-0">Profile</h6>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item preview-item">
-                  <div class="preview-thumbnail">
-                    <div class="preview-icon bg-dark rounded-circle">
-                      <i class="mdi mdi-settings text-success"></i>
-                    </div>
-                  </div>
-                  <div class="preview-item-content">
-                    <a class="admin_ms.html"> </a>
-                  </div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item preview-item">
-                  <div class="preview-thumbnail">
-                    <div class="preview-icon bg-dark rounded-circle">
-                      <i class="mdi mdi-logout text-danger"></i>
-                    </div>
-                  </div>
-                  <div class="preview-item-content">
-                    <a  href="index.html" class="preview-subject mb-1">Cerrar Session</a>
-                  </div>
-                </a>
-                <div class="dropdown-divider"></div>
-                <p class="p-3 mb-0 text-center">Advanced settings</p>
-              </div>
-            </li>
-          </ul>
-          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-            <span class="mdi mdi-format-line-spacing"></span>
-          </button>
-        </div>
-      </nav>
-        <!-- partial -->
-        <div class="main-panel">
-          <div class="content-wrapper">
-            <!--<center> <h1>Libro Diario</h1> </center>-->
-            <center><h1>Estado de Periodo H Tours Honduras</h1> </center>
-            <h5>________________________________________________________________________________________________________________</h5>
-            <div class="page-header">
-             <!--<center><h1> Libro Diario </h1></center>--> 
-             
-              <!-- <h1 class="page-title"> Nombre de la Tabla </h1> -->
-              <!-- <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Tables</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Basic tables</li>
-                </ol>
-              </nav> -->
-
-
-              
-                  <!-- INICIO MODAL PARA INGRESADA  -->
-           <div class="modal-container">
-           <div class="modal fade bd-example-modal-lg" id="dialogo1">
-                 <!-- COLOCARLE UN lg PARA TAMANO MEDIANO COLOCARLE UN sm PARA TAMANO PEQUENO -->
-            <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                 <!-- CABECERA DEL DIALOGO NUEVA-->
-            <div class="modal-header">
-            <h4 class="modal-title">Estado de la Cuenta</h4>
-                 <!-- <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button> -->
-            </div>
-                 <!-- CUERPO DEL DIALOGO INGRESADA -->
-            <div class="modal-body">
-            <center>
+        <!-- CUERPO DEL DIALOGO NUEVA -->
+        <div class="modal-body">
+          <center>
             <form action="" method="post">
-              <label class="form-label"> 
-                Seleccionar Periodo
-                <select class="form-select" aria-label="Default select example">
-                  <option value=""></option>
-                  <option value="">periodo-2020-ene-1-001</option>
-                  <option value="">periodo-2021-ene-1-002</option>
-                  <option value="">periodo-2022-ene-1-002</option>
-                </select>
-              </label>
-
-            <label class="form-label">
-              Estado
-              <input type='text' name='perido' class="form-control" required></input>
-              <button type="button" class="btn btn-success" data-dismiss="modal">Aceptar</button>
-            </label>
+              <Label>Seleccionar periodo</Label>
+              <select class="form-control text-white" name="" id="">
+                <option value="" selected></option>
+                <option value="">periodo-2020-ene-1-001</option>
+                <option value="">periodo-2021-ene-1-002</option>
+                <option value="">periodo-2022-ene-1-003</option>
+              </select>
+              <br>
+              <br>
+              <a href="" class="btn btn-secondary">Cancelar</a>
+              <button class="btn btn-primary">Aceptar</button>
             </form>
-            </div> 
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-            </center>
-            </div>
-            </div>
-            </div>
-            </div>
-                <!-- FIN DE MODAL PARA INGRESADA  -->
+        </div>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+        </center>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- FIN DE MODAL PARA NUEVA  -->
 
-                
-                  <!-- INICIO MODAL PARA PENDIENTE  -->
-           <div class="modal-container">
-            <div class="modal fade bd-example-modal-lg" id="dialogo2">
-                  <!-- COLOCARLE UN lg PARA TAMANO MEDIANO COLOCARLE UN sm PARA TAMANO PEQUENO -->
-             <div class="modal-dialog modal-sm">
-             <div class="modal-content">
-                  <!-- CABECERA DEL DIALOGO PENDIENTE-->
-             <div class="modal-header">
-             <h4 class="modal-title">Estado de la periodo</h4>
-                  <!-- <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button> -->
-             </div>
-              
-                 <!-- CUERPO DEL DIALOGO PENDIENTE -->
-                 <div class="modal-body">
-                  <center>
-                  <form action="" method="post">
-                  <label class="form-label">
-                    <label for="">Estado</label>
-                    
-                  <input type='text' name='perido' value="Activo" class="form-control" required></input>
-                  <button class="btn btn-success text-white">Aceptar</button>
-                  </label>
-             
+
       
+          <div class="content-wrapper">
+            <center> <h1>Estado de Resultado H Tours Honduras</h1> </center>
+            <h5>________________________________________________________________________________________________________________</h5>
+            <!-- <ul class="nav nav-pills nav-stacked">
+              <li class="active"><a href="#"></a></li>
+            </ul> -->
+            <center> <h1>Periodo-2021-ene-1-001</h1>
               
-      
-                  <!--<a href="" class="btn btn-secondary">Cancelar</a>
-                  <button type="submit" class="btn btn-primary">Registrar </button>-->
-                  </form>
-                  </div> 
-                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-                  </center>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                      <!-- FIN DE MODAL PARA PENDIENTE  -->
-             
-
-
-            
-
-
-
+              <!-- <h1 id="nombre-periodo"></h1> </center> -->
+              <!-- <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="#"></a></li>
+              </ul> -->
+              
+              
+              <center>
+                <br>
+                <h3> Desde: <input  type="date"  aria-label="Disabled input example" value="2021-01-01"  readonly >  Hasta: <input type="date" value="2021-12-31" readonly></h3>
+              </center>
+              <p align="right" valign="baseline">
                
-
+              </p>
+              
+              <p align="right" valign="baseline">
+                 
+                  <a type="button" href="libro_mayor.html" class="btn btn-success">Verificar</a> 
+                    <button type="button"  class="btn btn-success"  data-toggle="modal" data-target="#dialogo1">Periodo</button>
+                
+                <a type="button"  class="btn btn-success" href="javascript:window.print();">Generar PDF</a>
+              </p>
+            <ul class="nav nav-pills nav-stacked">
+              <li class="active"><a href="#"></a></li>
+              </ul>
+            <div class="page-header">
+              
             </div>
-            <!--<p align="right" valign="baseline">
-              <button type="button"  class="btn btn-success"  data-toggle="modal" data-target="#dialogo1">(+) Nuevo</button>
-            </p>-->
-            <p align="right" valign="baseline">
-              <button type="button"  class="btn btn-success"  data-toggle="modal" data-target="#dialogo1">Periodo</button> 
-           <a type="button"  class="btn btn-success" href="javascript:window.print();">Generar PDF</a>
-            </p>
             <div class="row">
-          
-              <div class="col-lg-12 stretch-card">
+             
+             
+              <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <!--<h4 class="card-title">Table with contextual classes</h4>-->
-                    <!--<p class="card-description"> Add class <code>.table-{color}</code>
-                    </p>-->
+                    <center><h3 class="card-title">Detalle</h3></center>
+                    <!-- <p class="card-description"> Add class <code>.table-striped</code> -->
+                    </p>
                     <div class="table-responsive">
-                      <table class="table table-bordered table-contextual"  >
+                      <table class="table table-condensed table-bordered table-hover ">
                         <thead>
-                          <tr>
-                            <th class="text-dark bg-white"> # </th>
-                            <th class="text-dark bg-white"> Nombre del periodo </th>
-                            <th class="text-dark bg-white"> Estado </th>
-                            <th class="text-dark bg-white"> Acciones </th>
+                           <tr class="text-dark">
+                            <th class="text-dark bg-gradient-secondary"><b>Ventas Netas</b></th>
+                            <th class="text-white bg-dark" >50,000</th>
+                            
                           </tr>
                         </thead>
                         <tbody>
-                          <tr class="text-white bg-dark">
-                            <td> 1 </td>
-                            <td>periodo-2020-ene-1-001</td>
-                            <td><div class="badge badge-outline-warning">Cerrado</div> </td> 
-                            <td><button type="button"  class="btn btn-info"  data-toggle="modal" data-target="#dialogo2">Editar</button> <button type="button"  class="btn btn-danger"  data-toggle="modal" data-target="#dialogo3">Eliminar</button> </td>
+                           <tr class="text-white bg-dark">
+                            <td class="text-dark bg-gradient-secondary"> <b>Costos de ventas</b> </td>
+                            <td>10,000</td>
+                             
                           </tr>
-                          <tr class="text-white bg-dark">
-                            <td> 2 </td>
-                            <td>Periodo-2021-ene-1-002 </td>
-                            <td><div class="badge badge-outline-warning">Cerrado</div> </td>
-                            <td><button type="button"  class="btn btn-info"  data-toggle="modal" data-target="#dialogo2">Editar</button> <button type="button"  class="btn btn-danger"  data-toggle="modal" data-target="#dialogo3">Eliminar</button> </td>
+                           <tr class="text-white bg-dark">
+                            <td class="text-dark bg-gradient-secondary"><B>Utilidad/perdida bruta</B></td>
+                            <td>40,000</td>
+                             
                           </tr>
-                          <tr class="text-white bg-dark">
-                            <td> 3 </td>
-                            <td> Periodo-2022-ene-1-003 </td>
-                            <td><div class="badge badge-outline-success">Activo</div></td>
-                            <td><button type="button"  class="btn btn-info"  data-toggle="modal" data-target="#dialogo2">Editar</button> <button type="button"  class="btn btn-danger"  data-toggle="modal" data-target="#dialogo3">Eliminar</button> </td>
-                            <!-- <td><button type="button"  class="btn btn-warning"  data-toggle="modal" data-target="#dialogo2">Pendiente</button>  -->
-                          </tr>
+                           <tr class="text-white bg-dark">
+                            <td class="text-dark bg-gradient-secondary" ><b> Total Gastos </b></td>
+                            <td>5,000</td>
+                           
+                            </tr>
+                            <tr class="text-white bg-dark">
+                              <td class="text-dark bg-gradient-secondary" ><b> Utilidad/Perdida Antes de impuestos </b></td>
+                              <td>10,000</td>
+                              
+                              </tr>
+                              <tr class="text-white bg-dark">
+                                <td class="text-dark bg-gradient-secondary" ><b>Impuesto a  utilidad </b></td>
+                                <td>5,250</td>
+                               
+                                </tr>
+                                <tr class="text-white bg-dark">
+                                  <td class="text-dark bg-gradient-secondary" ><b> Utilidad/Perdida Neta </b></td>
+                                  <td>29,750</td>
+                                  
+                                  </tr>
                         </tbody>
-                      </table>   
+                      </table>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+        
+
 
 @endsection
