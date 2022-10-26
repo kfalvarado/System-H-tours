26b1905331d063f5a6115b1b730cd612a8810ec3
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-25 20:53:46 -0600

    modified:   resources/views/librodiario/librodiario.blade.php

--- resources/views/librodiario/librodiario.blade.php
+++ resources/views/librodiario/librodiario.blade.php
@@ -518,7 +518,6 @@ Fabricio
                             <td> 3.1.1 </td>
                             <td> Aportaciones </td>
                             <td> 9000.00 </td>
-             
                             <td> <a href="" data-toggle="modal" data-target="#comprobante">   image1651861487718.jpeg  </a> </td>
                             <td> May 15, 2022 </td>
                             <td><button type="button"  class="btn btn-success"  data-toggle="modal" data-target="#dialogo7">Procesada</button></td>
