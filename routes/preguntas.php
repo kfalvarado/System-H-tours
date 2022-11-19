9ca8fe6c774bea8311d62a71ed700f8dd99c8282
Jeyson-M-C <jeyson.moran@outlook.com>
2022-11-18 22:38:45 -0600

    modified:   app/Http/Controllers/preguntas/PreguntasController.php 	modified:   app/Http/Controllers/usuarios/UsuariosController.php 	modified:   resources/views/preguntas/preguntas.blade.php 	modified:   resources/views/usuarios/usuarios.blade.php 	modified:   routes/preguntas.php 	modified:   routes/usuarios.php

--- routes/preguntas.php
+++ routes/preguntas.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\preguntas\PreguntasController;
  
 Route::get('/', [PreguntasController::class, 'mostrar'])->name('preguntas.inicio');
+ 
+Route::get('/actualizar', [PreguntasController::class, 'actualizar'])->name('preguntas.actualizar');
 
--- routes/usuarios.php
+++ routes/usuarios.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\usuarios\UsuariosController;
  
 Route::get('/', [UsuariosController::class, 'mostrar'])->name('usuarios.inicio');
+Route::get('/actualizar', [UsuariosController::class, 'actualizar'])->name('usuarios.actualizar');
 
 
 //Route::get('/mostrar',[UsuariosController::class,'mostrar'])->name('usuario.inicio');
