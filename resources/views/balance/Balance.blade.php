0e759a5cc1592d8102c763466a732cb89ba109f2
ZMLM20 <zoila.licona@unah.hn>
2022-11-20 07:15:03 -0600

    modified:   resources/views/balance/Balance.blade.php

--- resources/views/balance/Balance.blade.php
+++ resources/views/balance/Balance.blade.php
@@ -6,11 +6,16 @@ Balance General
 @endsection
 <!-- foto de la barra lateral debajo del nombre HTOURS  -->
 @section('foto-user1')
-{{ asset('assets/images/varon.png')}}
+@ if (cache::get('genero'))
+{{ asset('assets/images/varon.png')}
+@else
+{{ asset('assets/images/dama.png')}
+@endif
 @endsection
 
 <!-- nombre del usuario de la barra lateral  -->
 @section('Usuario-Lateral')
+{{ cache::get('user')}}
 Emerson
 @endsection
 <!-- rol del usuario de la barra lateral  -->
