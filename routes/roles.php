f9acb4f8bc618b0cbb43f88dd8d5464d20880762
Jeyson-M-C <jeyson.moran@outlook.com>
2022-11-20 23:20:34 -0600

    ins_roles

--- routes/roles.php
+++ routes/roles.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\roles\RolesController;
 // use App\Http\Controllers\home\SessionController;
 
 Route::get('/',[RolesController::class,'mostrar'])->name('mostrar.roles');
+Route::get('/ins_rols', [RolesController::class, 'insertar'])->name('roles.insertar');
 Route::get('/upd_rols', [RolesController::class, 'actualizar'])->name('roles.actualizar');
