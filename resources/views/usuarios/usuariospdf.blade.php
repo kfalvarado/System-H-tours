777b8c5833a2d3baa863c075d41b60995a68f67b
Jeyson-M-C <jeyson.moran@outlook.com>
2022-11-27 20:35:29 -0600

    pdf_usuarios

--- resources/views/usuarios/usuariospdf.blade.php
+++ resources/views/usuarios/usuariospdf.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
-    <title>Personas | PDF </title>
+    <title>Usuarios | PDF </title>
         <!-- ESTE CSS ES PARA OCULTAR DATOS EN LA IMPRESION-->
   <style>
     
@@ -21,81 +21,92 @@
 </head>
 
 <body>
-    
-    <img id="imagen"  style="float: right;" src="{{asset('assets\images\HTOURS.png')}}" alt="logo de Htours" height="100" width="100">
- 
-    <div class="d-grid gap-2 oculto-impresion">
-    <a class="btn btn btn-outline-dark" href="javascript:window.print();">Imprimir</a>
-    </div>
- 
-    <center>
+    <main class="container">
+       <div class="container-fluid mt-3 mb-3">
+        <div class="d-grid gap-2 oculto-impresion">
+            <a class="btn btn btn-outline-primary " href="javascript:window.print();"><h3>Imprimir</h3></a>
+        </div>
+       </div>
+        
 
-        <h1 id="titulo">Reporte Personas</h1>
-        <br>
-        <h2 id="fecha">Fecha:{{date('m/d/Y')}}</h2>
-        <br>
-        <table id="datos" >
-            <thead>
-                <th class="text-dark bg-white"># Codigo</th>
-                <th class="text-dark bg-white">Usuario</th>
-                <th class="text-dark bg-white">Nombre usuario</th>
-                <th class="text-dark bg-white">Estado</th>
-                <th class="text-dark bg-white">Rol</th>
-                <th class="text-dark bg-white">Tipo</th>
-                <th class="text-dark bg-white">Ultima Conexión</th>
-                {{-- <th class="text-dark bg-white">Preguntas contestadas</th> --}}
-                <th class="text-dark bg-white">Ingresos</th>
-                <th class="text-dark bg-white">Correo Electronico</th>
-            </thead>
-            <tbody>
-                @foreach ($usrArr as $usuario)
-          
+        <img id="imagen"  style="float: right;" src="{{asset('assets\images\HTOURS.png')}}" alt="logo de Htours" height="100" width="100">
+    
+     <div class="container">
+        <center>
+    
+            <h1 id="titulo">Reporte Usuarios</h1>
+            <br>
+            <h2 id="fecha">Fecha:{{date('m/d/Y')}}</h2>
+            <br>
+            <div class="table">
+                <table id="datos" class="table table-bordered table-contextual table_id" style=" border: 1px ridge black;">
+                    <thead>
+                        <th class="text-dark bg-white"># Codigo</th>
+                        <th class="text-dark bg-white">Usuario</th>
+                        <th class="text-dark bg-white">Nombre usuario</th>
+                        <th class="text-dark bg-white">Estado</th>
+                        <th class="text-dark bg-white">Rol</th>
+                        <th class="text-dark bg-white">Tipo</th>
+                        <th class="text-dark bg-white">Ultima Conexión</th>
+                        {{-- <th class="text-dark bg-white">Preguntas contestadas</th> --}}
+                        <th class="text-dark bg-white">Ingresos</th>
+                        <th class="text-dark bg-white">Correo Electronico</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($usrArr as $usuario)
                 
-                <tr>
-                    <td>{{$usuario['CODIGO_USUARIO']}}</td>
-                    <td>{{$usuario['USUARIO']}}</td>
-                    <td>{{$usuario['NOMBRE_USUARIO']}}</td>
-                    <td>{{$usuario['ESTADO_USUARIO']}}</td>
-                    <td>{{$usuario['COD_ROL']}}</td>
-                    <td>{{$usuario['TIPO']}}</td>
-                    <td>{{ substr($usuario['FECHA_ULTIMO_ACCESO'],0,10)}}</td>
-                    {{-- <td>{{$usuario['PREGUNTA_RESPONDIDA']}}</td> --}}
-                    <td>{{$usuario['PRIMER_ACCESO']}}</td>
-                    <td>{{$usuario['CORREO_ELECTRONICO']}}</td>
-                </tr>
-          @endforeach
-         
-            </tbody>
-        </table>
-        @if (count($usuario)> 12)
-
-        <img id="imagen"  style="float: right;position:relative;top: -760px;" src="{{asset('assets\images\HTOURS.png')}}" alt="logo de Htours" height="500" width="500">
+                        
+                        <tr>
+                            <td>{{$usuario['CODIGO_USUARIO']}}</td>
+                            <td>{{$usuario['USUARIO']}}</td>
+                            <td>{{$usuario['NOMBRE_USUARIO']}}</td>
+                            <td>{{$usuario['ESTADO_USUARIO']}}</td>
+                            <td>{{$usuario['COD_ROL']}}</td>
+                            <td>{{$usuario['TIPO']}}</td>
+                            <td>{{ substr($usuario['FECHA_ULTIMO_ACCESO'],0,10)}}</td>
+                            {{-- <td>{{$usuario['PREGUNTA_RESPONDIDA']}}</td> --}}
+                            <td>{{$usuario['PRIMER_ACCESO']}}</td>
+                            <td>{{$usuario['CORREO_ELECTRONICO']}}</td>
+                        </tr>
+                    
+                @endforeach
+                
+                    </tbody>
+                </table>
+            </div>
+            @if (count($usuario)> 12)
     
-        @endif
-    </center>
+            <img id="imagen"  style="float: right;position:relative;top: -760px;" src="{{asset('assets\images\HTOURS.png')}}" alt="logo de Htours" height="500" width="500">
+        
+            @endif
+        </center>
+     </div>
+        
+        
+        <script>
+            function imprimir() {
+                var titulo = document.getElementById('titulo').innerText,
+                    data = document.getElementById('datos').innerText,
+                    fecha = document.getElementById('fecha').innerText;
+                var img = document.getElementById('imagen');
     
+                var doc = new jsPDF();
+                doc.setFontSize(22);
+                doc.text('Empresa H Tours S. de R. L', 60, 10);
+                doc.setFontSize(22);
+                doc.text(titulo, 80, 20);
+                doc.setFontSize(11);
+                doc.text(fecha, 10, 30);
+                doc.setFontSize(16);
+                doc.addImage(img,150,10,60,30)
+                doc.text(data, 10, 60);
+                doc.save('Usuarios-Reporte.pdf');
+            }
+        </script>
     
-    <script>
-        function imprimir() {
-            var titulo = document.getElementById('titulo').innerText,
-                data = document.getElementById('datos').innerText,
-                fecha = document.getElementById('fecha').innerText;
-            var img = document.getElementById('imagen');
 
-            var doc = new jsPDF();
-            doc.setFontSize(22);
-            doc.text('Empresa H Tours S. de R. L', 60, 10);
-            doc.setFontSize(22);
-            doc.text(titulo, 80, 20);
-            doc.setFontSize(11);
-            doc.text(fecha, 10, 30);
-            doc.setFontSize(16);
-            doc.addImage(img,150,10,60,30)
-            doc.text(data, 10, 60);
-            doc.save('Reporte-Periodo.pdf');
-        }
-    </script>
-
+    </main>
+    
 </body>
 
 </html>
