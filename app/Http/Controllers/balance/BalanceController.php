a443038b06b17d4c535273c63430c2032f618121
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-29 23:04:00 -0600

    SEGURIDAD BALANCE

--- app/Http/Controllers/balance/BalanceController.php
+++ app/Http/Controllers/balance/BalanceController.php
@@ -14,72 +14,162 @@ class BalanceController extends Controller
     protected $url = 'http://localhost:3000';
     public function mostrar()
     {
-        $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
 
-        $personArr = $periodo->json();
-        $activoc = 0;
-        $activon = 0;
-        $pasivoc = 0;
-        $pasivon = 0;
-        $balanceArr = 0;
-        $patrimonio = 0;
-        return view("balance.Balance", compact('personArr','balanceArr','activoc','activon','pasivoc','pasivon','patrimonio'));
+        try {
+            //code...
+            $search = Http::withToken(Cache::get('token'))->post($this->url . '/permisos/sel_per_obj', [
+                "PV_ROL" => Cache::get('rol'),
+                "PV_OBJ" => "BALANCE"
+            ]);
+
+            $permisos = $search->json();
+            foreach ($permisos as $key) {
+                $consultar = $key['PER_CONSULTAR'];
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 'Error rol 24';
+        }
+
+        if (isset($consultar) == '1') {
+            try {
+                $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
+
+                $personArr = $periodo->json();
+                $activoc = 0;
+                $activon = 0;
+                $pasivoc = 0;
+                $pasivon = 0;
+                $balanceArr = 0;
+                $patrimonio = 0;
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'error PERMISOS 41';
+            }
+
+            try {
+                $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                    "USR" => Cache::get('user'),
+                    "ACCION" => 'PANTALLA BALANCE METODO GET',
+                    "DES" => Cache::get('user') . ' INGRESO A LA PANTALLA DE BALANCE',
+                    "OBJETO" => 'BALANCE'
+                ]);
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'ERROR BALANCE ';
+            }
+        } else {
+            $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                "USR" => Cache::get('user'),
+                "ACCION" => 'ACCESO NO AUTORIZADO BALANCE METODO GET',
+                "DES" => Cache::get('user') . ' INTENTO INGRESAR A LA PANTALLA DE BALANCE',
+                "OBJETO" => 'BALANCE'
+            ]);
+            return view('Auth.no-auth');
+        }
+
+        return view("balance.Balance", compact('personArr', 'balanceArr', 'activoc', 'activon', 'pasivoc', 'pasivon', 'patrimonio'));
     }
     public function insertar(Request $request)
     {
-        $balance = http::withToken(Cache::get('token'))->post($this->url.'/balance/insertar',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
-        $balanceArr=$balance->json();
-        // return $balanceArr;
-        $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
 
-        $personArr = $periodo->json();
+        try {
+            //code...
+            $search = Http::withToken(Cache::get('token'))->post($this->url . '/permisos/sel_per_obj', [
+                "PV_ROL" => Cache::get('rol'),
+                "PV_OBJ" => "BALANCE"
+            ]);
 
-        //activos corrientes 
-        $activoc = http::withToken(Cache::get('token'))->post($this->url.'/balance/activos_c',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
+            $permisos = $search->json();
+            foreach ($permisos as $key) {
+                $consultar = $key['PER_CONSULTAR'];
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 'Error rol 24';
+        }
 
-        // return $activoc;
-        $activoc = $activoc->json();
+        if (isset($consultar) == '1') {
+            try {
+                $balance = http::withToken(Cache::get('token'))->post($this->url . '/balance/insertar', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
+                $balanceArr = $balance->json();
+                // return $balanceArr;
+                $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
 
-        // return $activo_c;
+                $personArr = $periodo->json();
 
-        //activos no corrientes
+                //activos corrientes 
+                $activoc = http::withToken(Cache::get('token'))->post($this->url . '/balance/activos_c', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
 
-        $activon = http::withToken(Cache::get('token'))->post($this->url.'/balance/activos_n',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
-        $activon = $activon->json();
-        // return $activon;
+                // return $activoc;
+                $activoc = $activoc->json();
 
-        //pasivos corrientes
-        $pasivoc = http::withToken(Cache::get('token'))->post($this->url.'/balance/pasivos_c',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
-        $pasivoc = $pasivoc->json();
-        // return $pasivoc;
-        
-        //pasivos no corrientes
-        $pasivon = http::withToken(Cache::get('token'))->post($this->url.'/balance/pasivos_n',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
-        
-        $pasivon = $pasivon->json();
+                // return $activo_c;
 
-        // return $pasivoc;
+                //activos no corrientes
 
-        //patrimonio
-        $patrimonio = http::withToken(Cache::get('token'))->post($this->url.'/balance/patrimonio',[
-            'COD_PERIODO'=>$request->periodo
-        ]);
+                $activon = http::withToken(Cache::get('token'))->post($this->url . '/balance/activos_n', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
+                $activon = $activon->json();
+                // return $activon;
+
+                //pasivos corrientes
+                $pasivoc = http::withToken(Cache::get('token'))->post($this->url . '/balance/pasivos_c', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
+                $pasivoc = $pasivoc->json();
+                // return $pasivoc;
+
+                //pasivos no corrientes
+                $pasivon = http::withToken(Cache::get('token'))->post($this->url . '/balance/pasivos_n', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
+
+                $pasivon = $pasivon->json();
+
+                // return $pasivoc;
+
+                //patrimonio
+                $patrimonio = http::withToken(Cache::get('token'))->post($this->url . '/balance/patrimonio', [
+                    'COD_PERIODO' => $request->periodo
+                ]);
 
 
-        
-        $patrimonio = $patrimonio->json();
 
-        // return $patrimonio;
-        return view("balance.Balance", compact('balanceArr','personArr','activoc','activon','pasivoc','pasivon','patrimonio'));
+                $patrimonio = $patrimonio->json();
+
+                // return $patrimonio;
+
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'error PERMISOS 41';
+            }
+
+            try {
+                $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                    "USR" => Cache::get('user'),
+                    "ACCION" => 'PANTALLA BALANCE METODO GET',
+                    "DES" => Cache::get('user') . ' INGRESO A LA PANTALLA DE BALANCE',
+                    "OBJETO" => 'BALANCE'
+                ]);
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'ERROR BALANCE ';
+            }
+        } else {
+            $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                "USR" => Cache::get('user'),
+                "ACCION" => 'ACCESO NO AUTORIZADO BALANCE METODO GET',
+                "DES" => Cache::get('user') . ' INTENTO INGRESAR A LA PANTALLA DE BALANCE',
+                "OBJETO" => 'BALANCE'
+            ]);
+            return view('Auth.no-auth');
+        }
+        return view("balance.Balance", compact('balanceArr', 'personArr', 'activoc', 'activon', 'pasivoc', 'pasivon', 'patrimonio'));
     }
 }
