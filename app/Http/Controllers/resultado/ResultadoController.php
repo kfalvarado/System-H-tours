82bfc54c827daeeb0752e2ce83198a5d784347b0
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-29 23:08:44 -0600

    modified:   app/Http/Controllers/resultado/ResultadoController.php

--- app/Http/Controllers/resultado/ResultadoController.php
+++ app/Http/Controllers/resultado/ResultadoController.php
@@ -11,27 +11,112 @@ use Illuminate\Support\Facades\Http;
 
 class ResultadoController extends Controller
 {
-    
+
     protected $url = 'http://localhost:3000';
     public function mostrar()
     {
-        $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
-        $periodo = $periodo->json();
-        $resultado = 0;
-        return view("resultado.Resultado",compact('periodo','resultado'));
+        try {
+            //code...
+            $search = Http::withToken(Cache::get('token'))->post($this->url . '/permisos/sel_per_obj', [
+                "PV_ROL" => Cache::get('rol'),
+                "PV_OBJ" => "RESULTADOS"
+            ]);
+
+            $permisos = $search->json();
+            foreach ($permisos as $key) {
+                $consultar = $key['PER_CONSULTAR'];
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 'Error RESULTADOS 19';
+        }
+
+        if (isset($consultar) == '1') {
+            try {
+                $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
+                $periodo = $periodo->json();
+                $resultado = 0;
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'error RESULTADOS 41';
+            }
+
+            try {
+                $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                    "USR" => Cache::get('user'),
+                    "ACCION" => 'PANTALLA RESULTADOS METODO GET',
+                    "DES" => Cache::get('user') . ' INGRESO A LA PANTALLA DE RESULTADOS',
+                    "OBJETO" => 'RESULTADOS'
+                ]);
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'ERROR RESULTADOS ';
+            }
+        } else {
+            $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                "USR" => Cache::get('user'),
+                "ACCION" => 'ACCESO NO AUTORIZADO RESULTADOS METODO GET',
+                "DES" => Cache::get('user') . ' INTENTO INGRESAR A LA PANTALLA DE RESULTADOS',
+                "OBJETO" => 'RESULTADOS'
+            ]);
+            return view('Auth.no-auth');
+        }
+        return view("resultado.Resultado", compact('periodo', 'resultado'));
     }
 
     public function insertar(Request $request)
     {
-        //  return $request->periodo;
-        $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
-        $periodo = $periodo->json();
-        
-        $resultado = http::withToken(Cache::get('token'))->post($this->url . '/resultado',[
-            "PERIODO"=> $request->periodo
-        ]);
-        $resultado = $resultado->json();
+        try {
+            //code...
+            $search = Http::withToken(Cache::get('token'))->post($this->url . '/permisos/sel_per_obj', [
+                "PV_ROL" => Cache::get('rol'),
+                "PV_OBJ" => "RESULTADOS"
+            ]);
 
-        return view("resultado.Resultado",compact('periodo','resultado'));
+            $permisos = $search->json();
+            foreach ($permisos as $key) {
+                $consultar = $key['PER_CONSULTAR'];
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return 'Error RESULTADOS 19';
+        }
+
+        if (isset($consultar) == '1') {
+            try {
+                //  return $request->periodo;
+                $periodo = http::withToken(Cache::get('token'))->get($this->url . '/periodo');
+                $periodo = $periodo->json();
+
+                $resultado = http::withToken(Cache::get('token'))->post($this->url . '/resultado', [
+                    "PERIODO" => $request->periodo
+                ]);
+                $resultado = $resultado->json();
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'error RESULTADOS 41';
+            }
+
+            try {
+                $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                    "USR" => Cache::get('user'),
+                    "ACCION" => 'PANTALLA RESULTADOS METODO GET',
+                    "DES" => Cache::get('user') . ' INGRESO A LA PANTALLA DE RESULTADOS',
+                    "OBJETO" => 'RESULTADOS'
+                ]);
+            } catch (\Throwable $th) {
+                //throw $th;
+                return 'ERROR RESULTADOS ';
+            }
+        } else {
+            $bitacora = Http::withToken(Cache::get('token'))->post($this->url . '/seguridad/bitacora/insertar', [
+                "USR" => Cache::get('user'),
+                "ACCION" => 'ACCESO NO AUTORIZADO RESULTADOS METODO GET',
+                "DES" => Cache::get('user') . ' INTENTO INGRESAR A LA PANTALLA DE RESULTADOS',
+                "OBJETO" => 'RESULTADOS'
+            ]);
+            return view('Auth.no-auth');
+        }
+        return view("resultado.Resultado", compact('periodo', 'resultado'));
     }
 }
